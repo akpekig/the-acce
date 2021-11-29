942bb3fce7f223fdfa61c7c911e240bd493a4f82
akpekig <57150149+akpekig@users.noreply.github.com>
2021-11-29 17:29:40 +0000

    Register models to admin in Matters app

--- matters/admin.py
+++ matters/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Matter, Contact, Pretask, Location, Lawyer, Client
+admin.site.register(Matter)
+admin.site.register(Contact)
+admin.site.register(Pretask)
+admin.site.register(Location)
+admin.site.register(Lawyer)
+admin.site.register(Client)
