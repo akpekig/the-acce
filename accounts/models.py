7bd94cabddaae8562e8b913e6a901eecdf5066d1
akpekig <57150149+akpekig@users.noreply.github.com>
2021-11-29 21:35:12 +0000

    Create Account class in Accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.urls import reverse
+
+class Account(models.Model):
+    """Model extending built-in abstract class of User model."""
+    # Types of costs that the lawyer may charge.
+    class UserTypes(models.IntegerChoices):
+        LAWYER = 1, _("As a legal service")
+        CLIENT = 2, _("As a client")
+
+    key = models.UUIDField(default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    user_type = models.IntegerField(
+        choices=UserTypes.choices,
+        default=UserTypes.CLIENT,
+        help_text=("Select how you want to use this site."),
+    )
+    company = models.CharField(max_length=64, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, unique=True)
+    phone = models.CharField(max_length=64, blank=True, null=True, unique=True)
+    website = models.CharField(max_length=128, blank=True, null=True)
+    address = models.ForeignKey(
+        "Location",
+        models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of the model."""
+        return reverse("view", args=[str(self.key)])
+
+    def clean(self):
+        """Validates various fields of this model."""
+        if not (self.email and self.phone and self.website and self.address):
+            # Don't allow already due matters.
+            raise ValidationError(
+                "You need to have a way to be contacted in order to use this site!"
+            )
+
+class Location(models.Model):
+    pass
+
