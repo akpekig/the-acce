428ad820940e574b6f64b8ef773b993a1da2c181
akpekig <57150149+akpekig@users.noreply.github.com>
2021-11-29 21:36:01 +0000

    Register models in Accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, Client, Lawyer, Location
+
+admin.site.register(Account)
+admin.site.register(Client)
+admin.site.register(Lawyer)
+admin.site.register(Location)
